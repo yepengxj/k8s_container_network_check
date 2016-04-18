ad4a31e9b8e62e4c22ed326c3036869cfb1110ca
yepengxj <yepeng_xj@163.com>
2016-04-18 22:03:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 print "ETCD_HOST:$ETCD_HOST"
 print "ETCD_PORT:$ETCD_PORT"
-print "HOST_IP:$HOST_IP“
+print "HOST_IP:$HOST_IP"
 curl http://$ETCD_HOST:$ETCD_PORT/v2/keys/k8s_monitor/container_network_heathlecheck/$HOST_IP -XPUT -d value=$HOST_IP -d ttl=300
