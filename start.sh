9eefc6ee4ea431bb585fccb34436f2163a867375
yepengxj <yepeng_xj@163.com>
2016-04-18 22:15:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 env
-echo "ETCD_HOST:$ETCD_HOST"
-echo "ETCD_PORT:$ETCD_PORT"
-echo "HOST_IP:$HOST_IP"
+
 curl http://$USER_NAME:$USER_PASS@$ETCD_HOST:$ETCD_PORT/v2/keys/k8s_monitor/container_network_heathlecheck/$HOST_IP -XPUT -d value=$HOST_IP -d ttl=300
